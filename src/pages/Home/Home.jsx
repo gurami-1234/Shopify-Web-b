239a2cc4de93d8c38bf075e83df1d4d262f21443
gurami-1234 <guramidavitadze22@gmail.com>
2024-09-13 19:35:55 +0400

    loaders

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,47 +2,65 @@ import CartItem from "../../components/CartItem/CartItem";
 import ProductLayout from "../../layouts/ProductsLayout/ProductLayout";
 import { getAllProducts } from "../../helper/api";
 import { useEffect, useState } from "react";
-
+import { BounceLoader } from "react-spinners";
 
 
 function Home() {
     const [trendingProductList,setTrendingProductList] = useState([])
     const [newArrivalsProductList,setNewArrivalProductList] = useState([])
+    const [isLoadingTR,setIsLoadingTR] = useState(false)
+    const [isLoadingNew,setIsLoadingNew] = useState(false)
     useEffect(()=>{
-        getAllProducts(8,7).then((data)=>setTrendingProductList(data.products ))
-        getAllProducts(8,50).then((data)=>setNewArrivalProductList(data.products ))
+        setIsLoadingTR(true)
+        setIsLoadingNew(true)
+        getAllProducts(8,7)
+            .then((data)=>setTrendingProductList(data.products ))
+            .finally(()=>setIsLoadingTR(false))
+        getAllProducts(8,50)
+            .then((data)=>setNewArrivalProductList(data.products ))
+            .finally(()=>setIsLoadingNew(false))
+
     },[])
     return (   
         <main className="home-page">
             
             <ProductLayout layoutTitle="Trending Products">
                 {
-                    trendingProductList.map((el)=>
-                        <CartItem 
-                            id={el.id}
-                            key={el.id} 
-                            image={el.thumbnail}
-                            title={el.title}
-                            category={el.category} 
-                            discountPercentage={el.discountPercentage}
-                            price={el.price}
-                        />
+                    isLoadingTR?
+                        <BounceLoader size={100} color="#d8d8d8" cssOverride={{left:"50%",transform:"translateX(-50%)",position:"fixed"}}/>
+                        
+                        :(
+                        trendingProductList.map((el)=>
+                            <CartItem 
+                                id={el.id}
+                                key={el.id} 
+                                image={el.thumbnail}
+                                title={el.title}
+                                category={el.category} 
+                                discountPercentage={el.discountPercentage}
+                                price={el.price}
+                            />
+                        )
                     )
                 }
             </ProductLayout>
 
             <ProductLayout layoutTitle="New Arrival">
                 {
-                    newArrivalsProductList.map((el)=>
-                        <CartItem 
-                            id={el.id} 
-                            key={el.id}
-                            image={el.thumbnail}
-                            title={el.title}
-                            category={el.category} 
-                            discountPercentage={el.discountPercentage}
-                            price={el.price}
-                        />
+                    isLoadingNew?
+                    <BounceLoader size={100} color="#d8d8d8" cssOverride={{left:"50%",transform:"translateX(-50%)",position:"fixed"}}/>
+                    :(
+                        newArrivalsProductList.map((el)=>
+                            <CartItem 
+                                id={el.id} 
+                                key={el.id}
+                                image={el.thumbnail}
+                                title={el.title}
+                                category={el.category} 
+                                discountPercentage={el.discountPercentage}
+                                price={el.price}
+                            />
+                        )
                     )
                 }
             </ProductLayout>
