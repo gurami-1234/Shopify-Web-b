aaedac855ca6430365108b69072cae712a7af766
gurami-1234 <gukadavitadze22@gmail.com>
2024-10-04 20:35:25 +0400

    categories error

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -4,33 +4,48 @@ import { useContext, useEffect, useState } from "react";
 import { getProductsByCategory } from "helper/api";
 import { CartItem } from "components"; 
 import { ThemeContext } from "context";
+import { Link } from "react-router-dom";
+import { BounceLoader } from "react-spinners";
 import './Category.css'
 
 function Category() {
     const {categoryId} = useParams()
     const {isDark} = useContext(ThemeContext)
+    const [isLoading,setIsLoading]=useState(false)
     const [productsList,setProductsList] = useState([])
     useEffect(()=>{
+        setIsLoading(true)
         getProductsByCategory(categoryId)
             .then((data)=>setProductsList(data))
+            .finally(()=>setIsLoading(false))
     },[])
     return (  
         <main className={isDark?"category-page dark":"category-page"}>
-            <div className="title"><p>{categoryId}</p></div>
-            <ProductLayout>
-                {
-                    productsList.map((el)=>(
-                        <CartItem 
-                            title={el.title}
-                            price={el.price}
-                            discountPercentage={el.discountPercentage}
-                            image={el.thumbnail}
-                            id={el.id}
-
-                        />
-                    ))
-                }
-            </ProductLayout>
+            {
+                isLoading?<BounceLoader size={50} />:(
+                    <>
+                        <div className="title"><p>{categoryId}</p></div>
+                        {  
+                        productsList.length===0?<h1>Could not find products on this category <Link to="/categories">Back To Categories</Link></h1>:
+                            (<ProductLayout>
+                                {
+                                    productsList.map((el)=>(
+                                        <CartItem 
+                                            title={el.title}
+                                            price={el.price}
+                                            discountPercentage={el.discountPercentage}
+                                            image={el.thumbnail}
+                                            id={el.id}
+                
+                                        />
+                                    ))
+                                }
+                            </ProductLayout>)}
+                    </>
+                   
+                )
+            }
+           
         </main>
     );
 }
